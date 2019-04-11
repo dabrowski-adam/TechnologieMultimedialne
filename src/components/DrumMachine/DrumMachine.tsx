4f4d6dc754758de07857df1f15bba410a61b0b02
Michał Grzelak <grzelakm@outlook.com>
2019-04-11 12:18:15 +0200

    Code reformat -> deleted semicolons, added 4 spaces indent

--- src/components/DrumMachine/DrumMachine.tsx
+++ src/components/DrumMachine/DrumMachine.tsx
@@ -1,42 +1,42 @@
-import React, { Component, Fragment } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
-import './DrumMachine.scss';
+import React, {Component, Fragment} from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faPlayCircle} from '@fortawesome/free-solid-svg-icons'
+import './DrumMachine.scss'
 
-let Pizzicato = require('pizzicato/distr/Pizzicato');
+let Pizzicato = require('pizzicato/distr/Pizzicato')
 
 class DrumMachine extends Component {
 
-  sound: any;
+    sound: any
 
-  componentWillMount = (): void => {
-    this.sound = new Pizzicato.Sound({
-      source: 'wave',
-      options: {
-        type: 'sawtooth'
-      }
-    });
+    componentWillMount = (): void => {
+        this.sound = new Pizzicato.Sound({
+            source: 'wave',
+            options: {
+                type: 'sawtooth'
+            }
+        })
 
-    this.sound.on('play', () => {
-      setTimeout(() => {
-        this.sound.stop();
-      }, 1000);
-    });
-  }
+        this.sound.on('play', () => {
+            setTimeout(() => {
+                this.sound.stop()
+            }, 1000)
+        })
+    }
 
-  play = (): void => {
-    this.sound.play();
-  }
+    play = (): void => {
+        this.sound.play()
+    }
 
-  render() {
-    return (
-      <Fragment>
-        <div className='d-flex justify-content-center mt-3' onClick={this.play} >
-          <FontAwesomeIcon icon={faPlayCircle} size='9x' className='play-button' />
-        </div>
-      </Fragment>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <div className='d-flex justify-content-center mt-3' onClick={this.play}>
+                    <FontAwesomeIcon icon={faPlayCircle} size='9x' className='play-button'/>
+                </div>
+            </Fragment>
+        )
+    }
 }
 
-export default DrumMachine;
+export default DrumMachine
