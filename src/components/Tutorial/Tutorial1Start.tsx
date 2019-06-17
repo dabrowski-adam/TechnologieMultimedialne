af9bc4e5ef3246c321d1b67e0d47550e01f2f6a2
Mariusz <mariopl@poczta.onet.eu>
2019-06-17 18:14:06 +0200

    tut1, ma buga z hi hatem - zaznaczanie wielu

--- src/components/Tutorial/Tutorial1Start.tsx
+++ src/components/Tutorial/Tutorial1Start.tsx
@@ -7,35 +7,87 @@ import Tutorial from '../shared/Tutorial';
 const steps = [
   {
     selector: '',
-    content: `Let's make some music!`
+    content: `Hi! Welcome to our drum machine. We're going to teach you how to make some serious drum beats :d`
   },
   {
     selector: '.kick .preview',
-    content: 'Click on the instrument name to check out the sound it makes.'
+    content: `First off, let's start with the basics: the electronic drum kit consists of dozen or so synthesized drum sounds. The most basic ones are: the kick (click on it to check how it sounds)`
+  },
+  {
+    selector: '.snare .preview',
+    content: `The snare`
+  },
+  {
+    selector: '.clap .preview',
+    content: `The clap`
+  },
+  {
+    selector: '.closed-hat .preview',
+    content: `And the hi-hat`
+  },
+  {
+    selector: '',
+    content: `Great! This four sounds are enough to make our first beat. The type of beat we're going to make is called four-on-the-floor. Let's start with the kick drums!`
   },
   {
     selector: '.kick .beat-0',
-    content: 'Now click click here to select a note.'
+    content: `Click here to put the kick on a first beat`
   },
   {
     selector: '.kick .beat-4',
-    content: 'Select a few more to make a beat...'
+    content: `And the second one`
   },
   {
     selector: '.kick .beat-8',
-    content: '...and another one...'
+    content: `And the third one`
   },
   {
     selector: '.kick .beat-12',
-    content: '...and the last one.'
+    content: `And the fourth one`
   },
   {
     selector: '.play',
-    content: 'Now click play to hear the rhythm!'
+    content: `Great! And now you have four kicks for four-on-the-floor type beat! Press play to hear what you've just created`
+  },
+  {
+    selector: '',
+    content: `OK, sounding pretty good right now. But it's not finished yet. So now we're going to add in the snares`
+  },
+  {
+    selector: '.snare .beat-4',
+    content: `(I) They're going to be on the second and fourth beat`
+  },
+  {
+    selector: '.snare .beat-12',
+    content: `(II) They're going to be on the second and fourth beat`
+  },
+  {
+    selector: '',
+    content: `Great! Notice how our drums started going back-and-forth. The beats where you put your snares on are called the backbeat. The next thing we're going to add are claps`
+  },
+  {
+    selector: '.clap .beat-4',
+    content: `(I) Put them right under your snares`
+  },
+  {
+    selector: '.clap .beat-12',
+    content: `(II) Put them right under your snares`
+  },
+  {
+    selector: '',
+    content: `Perfect! What you've just done is called layering. It's one of key concepts in creating good sounding drum beats. It's when you layer two (or more) different drum sounds to achieve a new single sound. You can layer many different sounds together but in this example we're layered a snare and a clap to make a snareclap`
+  },
+  {
+    selector: '',
+    content: `The beat sounds pretty much done now but we're going to add one more thing. We're going to put hi-hats on all the steps from 1 to 16 just to fill-in some space and add some top-end`
+  },
+  {
+    selector: '.closed-hat .beat-0-15',
+    content: `Please put them on steps 1-16`
   },
   {
     selector: '.next',
-    content: 'You can now go to the next tutorial.'
+    content: `Great! And now the beat is finished. You can go and impress girls with it or click here to go to the next tutorial :d`
   }
 ];
 
@@ -61,15 +113,33 @@ const Tutorial1Start = () => {
         const { className } = e.target;
         if (step === 1 && className === 'preview') {
           changeStep(step + 1);
-        } else if (step === 2 && className === 'beat-0') {
+        } else if (step === 2 && className === 'preview') {
           changeStep(step + 1);
-        } else if (step === 3 && className === 'beat-4') {
+        } else if (step === 3 && className === 'preview') {
           changeStep(step + 1);
-        } else if (step === 4 && className === 'beat-8') {
+        } else if (step === 4 && className === 'preview') {
           changeStep(step + 1);
-        } else if (step === 5 && className === 'beat-12') {
+        } else if (step === 6 && className === 'beat-0') {
           changeStep(step + 1);
-        } else if (step === 6 && className === 'play') {
+        } else if (step === 7 && className === 'beat-4') {
+          changeStep(step + 1);
+        } else if (step === 8 && className === 'beat-8') {
+          changeStep(step + 1);
+        } else if (step === 9 && className === 'beat-12') {
+          changeStep(step + 1);
+        } else if (step === 10 && className === 'play') {
+          changeStep(step + 1);
+        } else if (step === 12 && className === 'beat-4') {
+          changeStep(step + 1);
+        } else if (step === 13 && className === 'beat-12') {
+          changeStep(step + 1);
+        } else if (step === 15 && className === 'beat-4') {
+          changeStep(step + 1);
+        } else if (step === 16 && className === 'beat-12') {
+          changeStep(step + 1);
+        } else if (step === 19 && className === 'beat-0-15') {
+          changeStep(step + 1);
+        } else if (step === 20 && className === 'next') {
           changeStep(step + 1);
         }
       }
@@ -78,8 +148,8 @@ const Tutorial1Start = () => {
   );
 
   const instruments = [
-    Instrument.OpenHat,
     Instrument.ClosedHat,
+    Instrument.Snare,
     Instrument.Clap,
     Instrument.Kick
   ];
